3b2b4bdd4c50a1c084632a548c31a2c43bf5a1de
rusel95 <ruslanpopesku95@gmail.com>
2017-07-27 13:22:44 +0300

    first commit

--- midiTest1/ViewController.swift
+++ midiTest1/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            let mixloop = try AKAudioFile(readFileName: "mixloop.wav", baseDir: .resources)
+            
+            let player = try AKAudioPlayer(file: mixloop) {
+                print("completion callback has been triggered!")
+            }
+            
+            AudioKit.output = player
+            AudioKit.start()
+            player.looping = true
+        } catch {
+            print(error)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
