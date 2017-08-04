43521f536eb08f780430a354d8e5bd045c817de1
rusel95 <ruslanpopesku95@gmail.com>
2017-08-04 12:33:34 +0300

    temp

--- midiTest1/ViewController.swift
+++ midiTest1/ViewController.swift
@@ -17,25 +17,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        do {
-            let mixloop = try AKAudioFile(readFileName: "mixloop.wav", baseDir: .resources)
+//        do {
+//            let mixloop = try AKAudioFile(readFileName: "mixloop.wav", baseDir: .resources)
+//            
+//            let player = try AKAudioPlayer(file: mixloop) {
+//                print("completion callback has been triggered!")
+//            }
+//            
+//            AudioKit.output = player
+//            AudioKit.start()
+//            player.looping = true
+//        } catch {
+//            print(error)
+//        }
+        
+        guard let mp3FileURL = Bundle.main.url(forResource: "Imagine Dragons - Battle Cry", withExtension: "mp3") else {
+            fatalError("\"test.mid\" file not found.")
+        }
+        
+        //transforming it to url
+        //instanciating asset with url associated file
+        let asset = AVAsset.init(url: mp3FileURL)
+        
+        print(asset.lyrics)
+        //using the asset property to get the metadata of file
+        for metaDataItems in asset.commonMetadata {
             
-            let player = try AKAudioPlayer(file: mixloop) {
-                print("completion callback has been triggered!")
+            //getting the title of the song
+            if metaDataItems.commonKey == "title" {
+                let titleData = metaDataItems.value as! String
+
+                print("title ---> \(titleData)")
             }
             
-            AudioKit.output = player
-            AudioKit.start()
-            player.looping = true
-        } catch {
-            print(error)
+            //getting the "Artist of the mp3 file"
+            if metaDataItems.commonKey == "artist" {
+                let artistData = metaDataItems.value as! String
+                print("artist ---> \(artistData)")
+            }
+            
+            //getting the thumbnail image associated with file
+//            if metaDataItems.commonKey == "artwork" {
+//                print(metaDataItems.value?.description)
+//            }
+            
+            if metaDataItems.commonKey == "comment" {
+                print(metaDataItems.value as! String)
+            }
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
 }
